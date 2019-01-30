b64b5b77683cfb2b02767b453dd8a58a5077c9f1
zunit <jackjiananzhou@gmail.com>
2019-01-29 19:49:53 -0500

    initial server upload

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now} - ${req.method} - ${req.url}`;
+
+  fs.appendFile('server.log', log + '\n',(err)=>{
+    if (err){
+      console.log('Unable to append to server.log.');
+    }
+  });
+  console.log(log);
+  next();
+});
+
+app.use((req, res, next)=>{
+  res.render('maintenance.hbs', {
+    pageTitle: 'Maintenance Page'
+  });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+// this is helper fuction returning the current year
+hbs.registerHelper('getCurrentYear', ()=>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res)=>{
+  // res.send('<h1>Hello Express!</h1>');
+  res.render('home.hbs',{
+    pageTitle: 'Home Page',
+    name: 'Jack',
+    welcomeMessage: 'Hello, how are you ',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs',{
+    pageTitle: 'About Page',
+  });
+});
+
+// Bad -- send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+
+app.listen(3000, ()=>{
+  console.log("server is up!");
+});
